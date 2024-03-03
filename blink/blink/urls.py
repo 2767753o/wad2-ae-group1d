243ebb8a7556bfd62989a2904d259a50e684468e
Varad Kulkarni <varadkulk123@gmail.com>
2024-03-03 15:39:56 +0000

    add about page

--- blink/blink/urls.py
+++ blink/blink/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import include
 from blink_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('blink/', include('blink_app.urls')),
 
     path('admin/', admin.site.urls),
 ]
