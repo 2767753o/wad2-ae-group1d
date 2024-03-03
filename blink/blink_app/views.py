c00c79bfff8e2412d1de264de991a594f2732ffe
Varad Kulkarni <varadkulk123@gmail.com>
2024-03-03 15:39:39 +0000

    add all views, comment out urls with parameters

--- blink/blink_app/views.py
+++ blink/blink_app/views.py
@@ -7,3 +7,51 @@ from django.urls import reverse
 
 def index(request):
     return render(request, 'blink/index.html')
+
+def login(request):
+    return render(request, 'blink/login.html')
+
+def resetPassword(request):
+    return render(request, 'blink/resetPassword.html')
+
+def register(request):
+    return render(request, 'blink/register.html')
+
+def friends(request):
+    return render(request, 'blink/friends.html')
+
+def view_post(request):
+    return render(request, 'blink/post.html')
+
+def view_likes(request):
+    return render(request, 'blink/likes.html')
+
+def search(request):
+    return render(request, 'blink/search.html')
+
+def create(request):
+    return render(request, 'blink/create.html')
+
+def settings(request):
+    return render(request, 'blink/settings.html')
+
+def about(request):
+    return render(request, 'blink/about.html')
+
+def help(request):
+    return render(request, 'blink/help.html')
+
+def view_user(request):
+    return render(request, 'blink/user.html')
+
+def user_analytics(request):
+    return render(request, 'blink/analytics.html')
+
+def user_followed_by(request):
+    return render(request, 'blink/followed_by.html')
+
+def user_following(request):
+    return render(request, 'blink/following.html')
+
+def user_logout(request):
+    return render(request, 'blink/logout.html')
