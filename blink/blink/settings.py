e3469d8333d6f06c2e958ffe9fa1dfef5e614312
Varad Kulkarni <varadkulk123@gmail.com>
2024-03-03 15:16:08 +0000

    add index view

--- blink/blink/settings.py
+++ blink/blink/settings.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -25,7 +27,10 @@ SECRET_KEY = 'btdd)1!yclbcat_ba(hmo)tqag(4pn%^=kx6)-y3@a)&j^p8xg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blinkwad2.eu.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'blinkwad2.eu.pythonanywhere.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'blink.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- blink/blink_app/views.py
+++ blink/blink_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+def index(request):
+    return render(request, 'blink/index.html')
