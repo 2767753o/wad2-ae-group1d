26eb95f119caf0bf99ad5b58e65d55535814e87c
Varad Kulkarni <varadkulk123@gmail.com>
2024-03-03 15:52:56 +0000

    add about us and help

--- blink/blink/urls.py
+++ blink/blink/urls.py
@@ -18,9 +18,12 @@ from django.urls import path
 from django.urls import include
 from blink_app import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('blink/', include('blink_app.urls')),
 
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
