2c498814ea4d8d79d29809e84e5feeb887fab70e
Varad Kulkarni <varadkulk123@gmail.com>
2024-03-14 17:00:08 +0000

    create posts working

--- blink/blink_app/forms.py
+++ blink/blink_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from blink_app.models import UserProfile
+from blink_app.models import UserProfile, Post
 
 
 class UserForm(forms.ModelForm):
@@ -15,3 +15,9 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('profilePicture',)
+
+
+class CreateForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('content', 'image',)
