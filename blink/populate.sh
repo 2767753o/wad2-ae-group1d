a3d93956ecb1f30c2f750963d5cbe406f2640814
Varad Kulkarni <varadkulk123@gmail.com>
2024-03-22 02:28:25 +0000

    remove commented code

--- blink/populate.sh
+++ blink/populate.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# rm db.sqlite3
-
-# ./manage.py makemigrations blink_app
-# ./manage.py migrate
-# ./manage.py loaddata fixtures
-
 rm db.sqlite3
 python3 manage.py makemigrations
 python3 manage.py migrate
