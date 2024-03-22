a87d573da818ac806f5913d6bc2e9e06ee1da0b9
Varad Kulkarni <varadkulk123@gmail.com>
2024-03-22 02:26:09 +0000

    population script working, can run manually or populate.sh

--- blink/populate.sh
+++ blink/populate.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# rm db.sqlite3
+
+# ./manage.py makemigrations blink_app
+# ./manage.py migrate
+# ./manage.py loaddata fixtures
+
 rm db.sqlite3
-
-./manage.py makemigrations blink_app
-./manage.py migrate
-./manage.py loaddata fixtures
-
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 populate.py
